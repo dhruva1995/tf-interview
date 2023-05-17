40b4a9b7b3d216b06fe73bb6adc55fea2c073557
Dhruva Chandra <helloworldiamnew@gmail.com>
2023-05-17 23:37:19 +0530

    FormattedDriver.java

--- demo/src/main/java/com/example/demo/Driver.java
+++ demo/src/main/java/com/example/demo/Driver.java
@@ -1,20 +1,21 @@
 package com.example.demo;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 
 public class Driver {
 
-
-
-
 	public static void main(String[] args) {
 		Map<String, List<Object>> inputs = new HashMap<>();
-		inputs.put("user", Arrays.asList("Dhruva", "Chandra"));
+		inputs.put("user", Arrays.asList("Dhruva", "Chandra", null));
 		inputs.put("dob", Arrays.asList(1, 2, 3, 4));
+		inputs.put("address", Arrays.asList());
 		
-		String template = "{ 'user': '$user', 'dob': '$dob' }";
+		String template = "{ 'user': '$user', 'dob': '$dob' , address: $address}";
 		
 		//Usage ...
 		TemplateIterator it = new TemplateIterator(template, inputs);
@@ -22,19 +23,5 @@ public class Driver {
 		while(it.hasNext()) {
 			System.out.println(it.next());
 		}
-
-		
-		
-		
-		
 	}
-	
-	
-
-	
-
-
-	
-
-
 }
